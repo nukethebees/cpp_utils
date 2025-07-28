59dd53712312c759cd85677357a2e0b9553dbeb6
Matthew Love <matthewlove94@gmail.com>
2025-07-28 22:12:45 +0100

    Add test for unequal split

--- tests/test_split_span.cpp
+++ tests/test_split_span.cpp
@@ -17,3 +17,13 @@ TEST(split_span, int_array_0_split_2) {
     EXPECT_TRUE(ml::iterables_equal(split[0], span_0.subspan(0, 3)));
     EXPECT_TRUE(ml::iterables_equal(split[1], span_0.subspan(3, 3)));
 }
+TEST(split_span, int_array_0_split_4) {
+    static constexpr std::size_t n{4};
+    auto const split{ml::split_span(span_0, n)};
+
+    ASSERT_EQ(split.size(), n);
+    EXPECT_TRUE(ml::iterables_equal(split[0], span_0.subspan(0, 2)));
+    EXPECT_TRUE(ml::iterables_equal(split[1], span_0.subspan(2, 2)));
+    EXPECT_TRUE(ml::iterables_equal(split[2], span_0.subspan(4, 1)));
+    EXPECT_TRUE(ml::iterables_equal(split[3], span_0.subspan(5, 1)));
+}
